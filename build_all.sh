81c68845f6b545bcdefb98cbab4553e31851bef4
Armin Altorffer <aapmak@gmail.com>
2014-12-15 11:53:58 +0100

    Parallel clean/build in build_all.sh Trivial change but it makes my life a little easier.

--- build_all.sh
+++ build_all.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
-mdtool build --configuration:dotNET --target:Clean
-mdtool build --configuration:'dotNET64|x64' --target:Clean
-mdtool build --configuration:Mono --target:Clean
+mdtool build --configuration:dotNET --target:Clean &
+mdtool build --configuration:'dotNET64|x64' --target:Clean &
+mdtool build --configuration:Mono --target:Clean &
 mdtool build --configuration:'Mono64|x64' --target:Clean
-mdtool build --configuration:dotNET --target:Build
-mdtool build --configuration:'dotNET64|x64' --target:Build
-mdtool build --configuration:Mono --target:Build
+mdtool build --configuration:dotNET --target:Build &
+mdtool build --configuration:'dotNET64|x64' --target:Build &
+mdtool build --configuration:Mono --target:Build &
 mdtool build --configuration:'Mono64|x64' --target:Build

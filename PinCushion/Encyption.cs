4aad0d4a5d1c9682bf9cbad566ce57ac72be0531
Armin Altorffer <aapmak@gmail.com>
2014-12-20 19:39:52 +0100

    Strengthened Crypto.Hash With each and every object already being stored with its own 1000 iteration Rfc2898 key and salt, the weakest link and most opportune target for an attack was the hash; although with it being SHA512 it already was hard to crack. But I felt a stronger hash was required so I decided to derive the key there as well, with a 10,000 iteration Rfc2898.

--- PinCushion/Encyption.cs
+++ PinCushion/Encyption.cs
@@ -85,43 +85,17 @@ namespace PinCushion
 		}
 
 		/*
-		 * A SHA512 mash hash generator
-		 *
-		 * This will mash together the input and the salt into a single array by even and odd positions.
-		 *
-		 * So:
-		 *
-		 * input_bytes will be placed at 0, 2, 4, 6, ..
-		 * salt_bytes will be placed at 1, 3, 5, 7, ..
-		 *
-		 * The hash is based on the mash.
+		 * Generates the hash, based on SHA512 and PBKDF2
 		 */
 		public static string Hash (string input, string salt)
 		{
-			byte[] salt_bytes = Encoding.UTF8.GetBytes (salt);
-			byte[] input_bytes = Encoding.UTF8.GetBytes (input);
-			byte[] combined_bytes = new byte[2 * (input_bytes.Length + salt_bytes.Length)];
-
-			short i = 0;
-
-			Action<byte> copyinput = new Action<byte> (delegate {
-				Array.Copy (input_bytes, i, combined_bytes, i * 2, 1);
-				i++;
-			});
-
-			Action<byte> copysalt = new Action<byte> (delegate {
-				Array.Copy (salt_bytes, i, combined_bytes, (i * 2) + 1, 1);
-				i++;
-			});
-
-			Array.ForEach (input_bytes, copyinput);
-			i = 0;
-			Array.ForEach (salt_bytes, copysalt);
-
-			List<byte> combined_list = new List<byte> ();
-			combined_list.AddRange (combined_bytes);
-			combined_list.RemoveAll (x => x == 0);                               // dump excess 0's (non-assigned bytes), clean-up
-			return Convert.ToBase64String (new SHA512Managed ().ComputeHash (combined_list.ToArray ()));
+			string result;
+			using (SHA512Managed sha512 = new SHA512Managed ()) {
+				byte[] salt_bytes = Encoding.UTF8.GetBytes (salt);
+				Rfc2898DeriveBytes derived = new Rfc2898DeriveBytes (input, salt_bytes, 10000);
+				result = Convert.ToBase64String (sha512.ComputeHash (derived.GetBytes (64)));
+			}
+			return result;
 		}
 	}
 }
